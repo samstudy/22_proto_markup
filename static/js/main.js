62517eeb034ef5755738f68c13ce31e85e12af45
sam156 <bskordo@gmail.com>
2017-07-11 00:00:22 -0400

    hided popover content,indentation

--- static/js/main.js
+++ static/js/main.js
@@ -1,15 +1,6 @@
 
 $(document).ready(function(){
-    $('[js-data-toggle="popover"]').popover({
-        placement : 'bottom',
-        html : true,
-        title : '<p><a href="#" class="close" data-dismiss="alert">×</a><p>',
-        content : '<div class="media"><div class="media-body"><h5 class="media-heading">Ваш регион-Новосибирск и область</h5>\
-		<div class="col-md-8"><h4 class="text-right">Угадали?</h4></div></div><div class="modal-footer col-md-9	">\
-		<button class="btn btn-default" type="submit">Да</button>\
-		<button class="btn btn-default" type="submit">Нет</button></div></div>'
-    })
-    $("[js-data-toggle=popover]").popover('show');
+    $("[js-data-toggle=dropdown]").popover('show');
     $(document).on("click", ".popover .close" , function(){
         $(this).parents(".popover").popover('hide');
     });
